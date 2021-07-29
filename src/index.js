0822fd325bd2f47bd6d307724cf388f2757cec02
Tabassum Khan <tabassum.kamal.k@gmail.com>
2021-07-29 18:21:47 +0530

    Initialising Credit Debit App with React

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const divElement = (
+    <div>
+        <h1>Tabassum Khan</h1>
+        <button className ="btn btn-block btn-lg btn-outline-primary">You Borrowed</button>
+    </div>
+);
+
+ReactDOM.render(divElement, document.getElementById("root"));
