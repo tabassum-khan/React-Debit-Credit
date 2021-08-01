a954c6c234b9674657edde870893ab3184c20b9b
Tabassum Khan <tabassum.kamal.k@gmail.com>
2021-08-01 18:17:09 +0530

    Add Updated Headbar with navbar and search bar

--- src/components/HeadBar.js
+++ src/components/HeadBar.js
@@ -3,14 +3,20 @@ import React from "react";
 
 const headbar = (
     <div className="headbar">
+        <ul className="nav-bar">
+            <li className="logo"><a href="/">Credit-Debit</a></li>
+            <li className="user">Welcome, Tabassum Khan</li>
+            <li className="logout"><a href="/">Logout</a></li>
+        </ul>
+
         <div className="search-bar">
-        <i className="fa fa-search" aria-hidden="true"></i>
-        <input id="search-input" className="form-control" placeholder="Search Transaction" autoComplete="off" spellCheck="false" autoCorrect="off" tabIndex="1"></input>
+            <i className="fa fa-search" aria-hidden="true"></i>
+            <input id="search-input" className="form-control" placeholder="Search Cutsomer..." autoComplete="off" spellCheck="false" autoCorrect="off" tabIndex="1"></input>
         </div>
 
-        <div className="transaction-btn">
+        <div className="customer-btn">
             <i className="fa fa-handshake-o" aria-hidden="true"></i>
-            <button className="btn btn-block">Add Transaction</button>
+            <button className="btn btn-block">Add Cutsomer</button>
         </div>
     </div>
 );
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,10 +2,7 @@ import React from "react";
 
 
 const navbar = (
-    <ul className="nav-bar">
-        <li className="logo"><a href="/">Debit-Credit</a></li>
-        <li className="logout"><a href="/">Logout</a></li>
-    </ul>
+    
 );
 
 function NavBar(){
