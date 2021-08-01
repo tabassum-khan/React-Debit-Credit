ee0cd2ca5f7a07761280f8facfa4be28e4bb6af7
Tabassum Khan <tabassum.kamal.k@gmail.com>
2021-08-01 17:19:13 +0530

    Updating headbar

---
+++ src/components/HeadBar.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+const headbar = (
+    
+);
+
+function HeadBar(){
+    return headbar;
+}
+
+export default HeadBar;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,10 +2,22 @@ import React from "react";
 
 
 const navbar = (
-    <ul className="nav-bar">
-        <li className="logo"><a href="">Debit-Credit</a></li>
-        <li className="logout"><a href="">Logout</a></li>
-    </ul>
+    <div className="headbar">
+        <ul className="nav-bar">
+            <li className="logo"><a href="">Debit-Credit</a></li>
+            <li className="user"><a href="">Welcome, Tabassum Khan</a></li>
+            <li className="logout"><a href="">Logout</a></li>
+        </ul>
+        <div className="search-bar">
+        <i className="fa fa-search" aria-hidden="true"></i>
+        <input id="search-input" className="form-control" placeholder="Search Transaction" autoComplete="off" spellCheck="false" autoCorrect="off" tabIndex="1"></input>
+        </div>
+
+        <div className="transaction-btn">
+            <i className="fa fa-handshake-o" aria-hidden="true"></i>
+            <button className="btn btn-block">Add Transaction</button>
+        </div>
+    </div>
 );
 
 function NavBar(){
